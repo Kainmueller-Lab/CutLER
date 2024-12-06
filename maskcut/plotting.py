d385131ad00037536d271dd50b094dfadba48781
Mardt, Andreas <andreasmardt@gmail.com>
2024-12-06 16:47:20 +0100

    changed position matrix, added iterative scheme

--- maskcut/plotting.py
+++ maskcut/plotting.py
@@ -9,10 +9,11 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from scipy.linalg import eigh
 
-from utils.matrix import construct_distant_penalty_matrix
 
 sys.path.append('../')
 from third_party.TokenCut.unsupervised_saliency_detection import utils
+from utils.matrix import construct_distant_penalty_matrix
+
 
 # Image transformation applied to all images
 ToTensor = transforms.Compose([transforms.ToTensor(),
@@ -58,13 +59,14 @@ def shift(A):
     return A / 2
 
 
-def get_affinity_matrix(feats, act=set_min):
+def get_affinity_matrix(feats, act=set_min, sigma=2, sqrt=True, double_sqrt=True):
     # get affinity matrix via measuring patch-wise cosine similarity
     feats = F.normalize(feats, p=2, dim=0)
     A = (feats.transpose(0, 1) @ feats).cpu().numpy()
     # convert the affinity matrix to a binary one.
     A = A.astype(np.double)
-    A = act(A) + construct_distant_penalty_matrix(np.sqrt(A.shape[0]), sigma=4)
+    distance_pen = construct_distant_penalty_matrix(np.sqrt(A.shape[0]), sigma=sigma, sqrt=sqrt, double_sqrt=double_sqrt)
+    A = act(A) * distance_pen
     d_i = np.sum(A, axis=1)
     D = np.diag(d_i)
     T = A / np.sum(A, axis=1, keepdims=True)
@@ -94,35 +96,38 @@ def main():
     if CUDA:
         backbone.cuda()
 
-    for (names, act) in [['set_min', set_min], ['softmax', softmax], ['softmax_3T', softmax_3T],
-                         ['softmax_halfT', softmax_halfT], ['shift', shift]]:
+    for (names, act) in [['softmax', softmax]]:  # [['set_min', set_min], ['softmax', softmax], ['softmax_3T', softmax_3T],
+                        #  ['softmax_halfT', softmax_halfT], ['shift', shift]]
         for i in range(1, 9):
             input_image = f'demo{i}.jpg'
             img_path = 'imgs/' + input_image
             m_list = [2, 3, 5, 7, 10]
             n_s = len(m_list)
-            n_figures = max(m_list)
+            n_figures = max(m_list) + 1
             feats = get_feat(img_path=img_path, backbone=backbone, patch_size=PATCHSIZE)
-            T, A, D = get_affinity_matrix(feats, act=act)
-            size = T.shape[0]
-            eigvals_origin, _ = eigh(A, D, subset_by_index=[size - max(m_list) - 4, size - 2])
-            figure, axes = plt.subplots(n_s, n_figures, figsize=(n_figures * 4, n_s * 4))
-            axes[0, -1].set_title('Eivals')
-            axes[0, -1].plot(eigvals_origin, '.')
-            axes[0, -1].set_yscale('log')
-            for m_i, m in enumerate(m_list):
-                Pcca_m = pcca(T, m)
-                T_m = Pcca_m.coarse_grained_transition_matrix
-                member = Pcca_m.memberships
-                eigvals, eigvec = np.linalg.eig(T_m)
-                sort_id = np.argsort(eigvals)
-                second_larg_id = sort_id[-2]
-                sort_second = np.argsort(eigvec[:, second_larg_id])
+            for sigma in [45,50,55,60,65,70,75,80,85,90,95,100]: # [4,8,10,12]
+                T, A, D = get_affinity_matrix(feats, act=act, sigma=sigma, sqrt=False, double_sqrt=False)
+                size = T.shape[0]
+                eigvals_origin, _ = eigh(A, D, subset_by_index=[size - max(m_list) - 4, size - 2])
+                figure, axes = plt.subplots(n_s, n_figures, figsize=(n_figures * 4, n_s * 4))
+                axes[0, 2].set_title('Eivals')
+                axes[0, 2].plot(eigvals_origin, '.')
+                axes[0, 2].set_yscale('log')
+                for m_i, m in enumerate(m_list):
+                    Pcca_m = pcca(T, m)
+                    T_m = Pcca_m.coarse_grained_transition_matrix
+                    member = Pcca_m.memberships
+                    eigvals, eigvec = np.linalg.eig(T_m)
+                    sort_id = np.argsort(eigvals)
+                    second_larg_id = sort_id[-2]
+                    sort_second = np.argsort(eigvec[:, second_larg_id])
 
-                for ind_i, ind in enumerate(sort_second):
-                    axes[m_i, ind_i].set_title('Eigvec: {:.3}'.format(eigvec[ind, second_larg_id]))
-                    axes[m_i, ind_i].imshow(member[:, ind].reshape(60, 60))
-            plt.savefig('./Results/res_{}_{}'.format(names, input_image))
+                    for ind_i, ind in enumerate(sort_second):
+                        axes[m_i, ind_i].set_title('Eigvec: {:.3}'.format(eigvec[ind, second_larg_id]))
+                        axes[m_i, ind_i].imshow(member[:, ind].reshape(60, 60))
+                    hard_assignment = np.argmax(member, axis=-1)
+                    axes[m_i, -1].imshow(hard_assignment.reshape(60,60))
+                plt.savefig('./Results/res_{}_{}_{}'.format(names, sigma, input_image))
 
 
 if __name__ == '__main__':
--- utils/matrix.py
+++ utils/matrix.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def construct_distant_penalty_matrix(patch_dimension, sigma=4):
+def construct_distant_penalty_matrix(patch_dimension, sigma=4, sqrt=True, double_sqrt=True):
     """Construct a matrix that penalizes distant pixels in a patch.
 
         Parameters
@@ -22,7 +22,11 @@ def construct_distant_penalty_matrix(patch_dimension, sigma=4):
     xv, yv = np.meshgrid(x, y, indexing='ij')
     xv = xv.reshape(-1, 1)
     yv = yv.reshape(-1, 1)
-
-    R = np.exp(-((xv - xv.T) ** 2 + (yv - yv.T) ** 2) / (2 * sigma ** 2))
+    nom = (xv - xv.T) ** 2 + (yv - yv.T) ** 2
+    if sqrt:
+        nom = np.sqrt(nom) 
+        if double_sqrt:
+            nom = np.sqrt(nom)
+    R = np.exp(-nom/ (2 * sigma ** 2))
 
     return R  # (patch_dimension ** 2, patch_dimension ** 2)
